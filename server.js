5525cf103d7d260f99ec5fe63174ea9c511ec90a
Akira <akira_laine@outlook.com>
2015-12-30 11:26:40 +0000

    removed unnecessary stuff

--- server.js
+++ server.js
@@ -1,23 +1,13 @@
 'use strict';
 
 var express = require('express');
-var mongoose = require('mongoose');
 var session = require('express-session');
 var route = require("./app/routes.js");
 var stocks = ["FB", "AMZN"];
 var app = express();
-require('dotenv').load();
-
-mongoose.connect(process.env.MONGO_URI);
 
 app.use('/controllers', express.static(process.cwd() + '/app/controllers'));
 app.use('/public', express.static(process.cwd() + '/public'));
-
-app.use(session({
-	secret: 'secretClementine',
-	resave: false,
-	saveUninitialized: true
-}));
 
 route(app);
 
